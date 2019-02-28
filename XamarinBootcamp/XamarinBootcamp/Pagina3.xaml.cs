3a129e57cfc1368c7516dadf464a9b067ffc836e
Patrick Roos <patrick.roos@kpn.com>
2019-02-28 16:06:24 +0100

    Bootcamp deel 1

---
+++ XamarinBootcamp/XamarinBootcamp/Pagina3.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinBootcamp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Pagina3 : ContentPage
+	{
+		public Pagina3 ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
