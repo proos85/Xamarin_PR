d0d35346de45905a33b6ebb69d7aa487a250e08e
Patrick Roos <patrick.roos@kpn.com>
2019-03-01 18:34:29 +0100

    Triggers

---
+++ XamarinBootcamp/XamarinBootcamp/ValueConverters/LoginButtonMandatoryFieldsConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace XamarinBootcamp.ValueConverters
+{
+    public class LoginButtonMandatoryFieldsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!int.TryParse(value?.ToString() ?? string.Empty, out int numChars))
+            {
+                return false;
+            }
+
+            return numChars > 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
